7f8c137772b6141f02b77d7e6c14352e145ad271
Danilo Amoroso <dan.j.amoroso@gmail.com>
2020-12-07 11:34:31 +0100

    Move topic config from hashmap to TopicConfig struct

--- src/config.rs
+++ src/config.rs
@@ -1,12 +1,10 @@
-use std::collections::HashMap;
-
 #[derive(Clone, Debug)]
 pub struct Config {
     pub wait_interval: u64,
     pub base_path: String,
     pub camunda_username: String,
     pub camunda_password: String,
-    pub topics: Vec<HashMap<String, String>>,
+    pub topics: Vec<TopicConfig>,
     pub lock_duration: Option<i64>,
     pub worker_id: String,
 }
@@ -16,7 +14,7 @@ impl Config {
         camunda_base_url: String,
         camunda_username: String,
         camunda_password: String,
-        topics: Vec<HashMap<String, String>>,
+        topics: Vec<TopicConfig>,
         worker_id: String,
     ) -> Self {
         Self {
@@ -27,6 +25,21 @@ impl Config {
             topics,
             lock_duration: Some(60i64),
             worker_id,
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct TopicConfig {
+    pub topic_id: String,
+    pub wait_interval: Option<u64>,
+}
+
+impl TopicConfig {
+    pub fn new(topic_id: String) -> Self {
+        Self {
+            topic_id,
+            wait_interval: None,
         }
     }
 }
